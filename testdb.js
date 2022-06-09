1fd93f5cb1aca472d76a30d5c93fb1e3664b7136
J-a-yy <mvk1407@gmail.com>
2022-06-09 09:50:08 +0530

    graphQL basic query checks

---
+++ testdb.js
@@ -0,0 +1,27 @@
+export const users = [
+{
+    id: "23131",
+    firstName: "John",
+    lastName: "Wick",
+    email: "john@example.com",
+    password: "12345"
+},
+{
+    id: "43213",
+    firstName: "Johny",
+    lastName: "Wick",
+    email: "john@example.com",
+    password: "123457"
+}
+]
+
+export const quotes = [
+    {
+        name: "this is test quote 1",
+        by: "23131"
+    },
+    {
+        name: "this is test quote 2",
+        by: "43213"
+    }
+]
